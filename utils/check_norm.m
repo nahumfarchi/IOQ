79b0cb1cbda1f50659d36b57c4fe011b2191f386
nahumf <nahumf@csf.technion.ac.il>
2018-03-20 16:35:39 +0200

    minor

--- utils/check_norm.m
+++ utils/check_norm.m
@@ -3,22 +3,18 @@ function [pass, nrm_out] = check_norm(str1, str2, varargin)
 %   Detailed explanation goes here
 
     p = inputParser;
-    addOptional(p, 'Tol', 1e-10);
-    addOptional(p, 'Log', []);
+    addOptional(p, 'Tol', 1e-7);
+    addOptional(p, 'Log', -1);
     parse(p, varargin{:});
     
     TOL = p.Results.Tol;
-    if ~isempty(p.Results.Log)
-        log = p.Results.Log;
-    else
-        log = [];
-    end
+    log = p.Results.Log;
 
     res1 = evalin('caller', str1);
     res2 = evalin('caller', str2);
 
     nrm = norm(res1 - res2);
-    msg = ['norm( ', str1, ' - ', str2, ' ) < ', num2str(TOL)];
+    msg = ['norm( [ ', str1, ' ] - [ ', str2, ' ] ) < ', num2str(TOL)];
     if ~isempty(log)
         log_and_print(log, 'Checking that %s...\n', msg);
         log_and_print(log, ['( = ', num2str(nrm), ')\n']);
